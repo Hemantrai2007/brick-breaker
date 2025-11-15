52b28ca1db3738f76879584fe56e26b9c32253f9
HEMANT RAI <ramvitoli973@gmail.com>
2025-11-16 00:37:41 +0530

    Add files via upload

---
+++ Gameplay.java
@@ -0,0 +1,207 @@
+package brickBracker;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class Gameplay extends JPanel implements KeyListener, ActionListener {
+    private boolean play = false;
+    private int score = 0;
+    private final int TOTAL_BRICKS = 21;
+    private int totalBricks = TOTAL_BRICKS;
+
+    private final Timer timer;
+    private final int delay = 8;
+
+    private int playerX = 310;
+
+    private int ballposX = 120;
+    private int ballposY = 350;
+    private int ballXdir = -1;
+    private int ballYdir = -2;
+
+    private MapGenerator map;
+
+    public Gameplay() {
+        map = new MapGenerator(3, 7);
+        timer = new Timer(delay, this);
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
+        timer.start();
+        addKeyListener(this);
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        // background
+        g.setColor(Color.black);
+        g.fillRect(1, 1, 692, 592);
+
+        // draw map
+        map.draw((Graphics2D)g);
+
+        // borders
+        g.setColor(Color.yellow);
+        g.fillRect(0, 0, 3, 592);
+        g.fillRect(0, 0, 692, 3);
+        g.fillRect(691, 0, 3, 592);
+
+        // paddle
+        g.setColor(Color.green);
+        g.fillRect(playerX, 550, 100, 8);
+
+        // ball
+        g.setColor(Color.yellow);
+        g.fillOval(ballposX, ballposY, 20, 20);
+
+        // score
+        g.setColor(Color.white);
+        g.setFont(new Font("serif", Font.BOLD, 25));
+        g.drawString("" + score, 590, 30);
+
+        // win condition
+        if(totalBricks <= 0) {
+            play = false;
+            ballXdir = 0;
+            ballYdir = 0;
+            g.setColor(Color.green);
+            g.setFont(new Font("serif", Font.BOLD, 30));
+            g.drawString("You Won! Score: " + score, 190, 300);
+            
+            g.setFont(new Font("serif", Font.BOLD, 20));
+            g.drawString("Press Enter to Restart", 230, 350);
+        }
+        
+        // lose condition
+        if(ballposY > 570) {
+            play = false;
+            ballXdir = 0;
+            ballYdir = 0;
+            g.setColor(Color.red);
+            g.setFont(new Font("serif", Font.BOLD, 30));
+            g.drawString("Game Over! Score: " + score, 190, 300);
+            
+            g.setFont(new Font("serif", Font.BOLD, 20));
+            g.drawString("Press Enter to Restart", 230, 350);
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        timer.start();
+
+        if (play) {
+            ballposX += ballXdir;
+            ballposY += ballYdir;
+
+            // left border
+            if (ballposX < 0) {
+                ballXdir = -ballXdir;
+            }
+
+            // top border
+            if (ballposY < 0) {
+                ballYdir = -ballYdir;
+            }
+
+            // right border
+            if (ballposX > 670) {
+                ballXdir = -ballXdir;
+            }
+
+            // paddle collision
+            if (new Rectangle(ballposX, ballposY, 20, 20)
+                    .intersects(new Rectangle(playerX, 550, 100, 8))) {
+                ballYdir = -ballYdir;
+            }
+
+            // brick collision
+            A: for(int i = 0; i < map.map.length; i++) {
+                for(int j = 0; j < map.map[0].length; j++) {
+                    if(map.map[i][j] > 0) {
+                        int brickX = j * map.brickWidth + 80;
+                        int brickY = i * map.brickHeight + 50;
+                        int brickWidth = map.brickWidth;
+                        int brickHeight = map.brickHeight;
+
+                        Rectangle rect = new Rectangle(brickX, brickY, brickWidth, brickHeight);
+                        Rectangle ballRect = new Rectangle(ballposX, ballposY, 20, 20);
+
+                        if(ballRect.intersects(rect)) {
+                            map.setBrickValue(0, i, j);
+                            totalBricks--;
+                            score += 5;
+
+                            if(ballposX + 19 <= rect.x || ballposX + 1 >= rect.x + rect.width) {
+                                ballXdir = -ballXdir;
+                            } else {
+                                ballYdir = -ballYdir;
+                            }
+                            break A;
+                        }
+                    }
+                }
+            }
+
+            repaint();
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {}
+    @Override
+    public void keyReleased(KeyEvent e) {}
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            if (playerX >= 600) {
+                playerX = 600;
+            } else {
+                moveRight();
+            }
+        }
+        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            if (playerX <= 10) {
+                playerX = 10;
+            } else {
+                moveLeft();
+            }
+        }
+        if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+            if (!play) {
+                play = true;
+                ballposX = 120;
+                ballposY = 350;
+                ballXdir = -1;
+                ballYdir = -2;
+                playerX = 310;
+                score = 0;
+                totalBricks = TOTAL_BRICKS;
+                map = new MapGenerator(3, 7);
+
+                repaint();
+            }
+        }
+    }
+
+    public void moveRight() {
+        play = true;
+        playerX += 20;
+    }
+
+    public void moveLeft() {
+        play = true;
+        playerX -= 20;
+    }
+}
